cd67ce783b6a1e6241e08c9966f7a898904d9e90
SantoSarker101 <santosarker404@gmail.com>
2024-01-20 05:08:17 +0600

    Created Delete Operation and Finished delete operations work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -56,7 +56,22 @@ async function run() {
       const result = await ToyCollection.find(query).toArray();
       res.send(result);
     })
-    
+
+
+    app.get('/toysInfos/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await ToyCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
+    app.delete('/toysInfos/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await ToyCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
